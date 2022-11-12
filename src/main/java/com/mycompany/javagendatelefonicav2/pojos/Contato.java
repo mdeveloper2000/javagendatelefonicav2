779c08b088b484306c23e6857a2954816218d55d
darkbasic2000 <marcio.vieira.scea@gmail.com>
2022-11-12 14:39:58 -0400

    Classe POJO Contato

---
+++ src/main/java/com/mycompany/javagendatelefonicav2/pojos/Contato.java
@@ -0,0 +1,60 @@
+package com.mycompany.javagendatelefonicav2.pojos;
+
+public class Contato {
+
+    private int id;
+    private String nome;
+    private String telefone;
+    private String email;
+    private String foto;
+    private String anotacoes;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getAnotacoes() {
+        return anotacoes;
+    }
+
+    public void setAnotacoes(String anotacoes) {
+        this.anotacoes = anotacoes;
+    }
+    
+}
